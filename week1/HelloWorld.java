e080da9188e4968735d37c18647b705d7b20201f
Esmeralda Amado <amadoesmeralda6@gmail.com>
2024-09-29 17:38:14 -0700

    Add files via upload

---
+++ week1/HelloWorld.java
@@ -0,0 +1,13 @@
+/* Name: Esmeralda Amado 
+ * Course: CS2011
+ * Section: 09 & 10
+ * Description: My fist java program that prints out 'Hello World'
+ */
+
+public class HelloWorld{
+    public static void main(String []args){
+        System.out.println("Hello World!");
+        System.out.println("I like spaghetti!");
+    }
+}
+
